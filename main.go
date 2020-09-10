57cc6e8e260c53c03f3ce934f0a0bdb398830a02
chenyan <chengyan605@163.com>
2020-09-10 17:15:58 +0800

    取出调试代码

--- main.go
+++ main.go
@@ -6,36 +6,11 @@ import (
 	"github.com/astaxie/beego"
 )
 
-/*
-type Student struct {
-	Id    int // 主键
-	Name  string
-	Age   int
-	Sex   string
-	Score float32
-	Addr  string
-}*/
-
 func main() {
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
 
-	// 初始化dbmanager
-	//Init()
-	/*
-		o := orm.NewOrm()
-		o.Using("default")
-		stu := new(Student)
-
-		stu.Name = "bei"
-		stu.Age = 25
-		stu.Sex = "m"
-		stu.Score = 88
-		stu.Addr = "hunan.leiyang"
-
-		fmt.Println(o.Insert(stu))
-	*/
 	beego.Run()
 }
